cfdf9f358dab730def9e5653daf66325ed5b2a17
Loga21 <prabha12738@gmail.com>
2023-12-04 19:22:36 +0530

    updated

--- src/pages/HomePage/MyProfile/MyProfile.js
+++ src/pages/HomePage/MyProfile/MyProfile.js
@@ -21,7 +21,8 @@ class MyProfile extends Component {
     return (
       <>
         <h3>Profile Info</h3>
-        <p>Update Profile Name :</p>
+        <p>Update Profile Name : </p>
+        <h5>hi</h5>
         <input type='text' value={this.state.profileName} onChange={this.handleProfileNameChange} />
         <p>{this.state.profileName}</p>
       </>
